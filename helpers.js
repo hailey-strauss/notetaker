9585eba4ab4622dbe130a6a1507d0b4111167fe4
Hailey Strauss <hstrauss2001@gmail.com>
2023-11-14 12:47:04 -0500

    added files

---
+++ helpers.js
@@ -0,0 +1,8 @@
+// Packages
+const fs = require("fs");
+const util = require("util");
+
+const readFromFile = util.promisify(fs.readFile);
+const writeToFile = util.promisify(fs.writeFile);
+
+module.exports = { readFromFile, writeToFile };
---
+++ server.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const path = require("path");
+const { readFromFile, writeToFile } = require("./helpers");
+const uuid = require("uuid");
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+app.use(express.static("public"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// get index
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/index.html"));
+});
+
+// get notes
+app.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/notes.html"));
+});
+
+app.get("/api/notes", async (req, res) => {
+  let notes = await readFromFile("db/db.json");
+  let parsedNotes = JSON.parse(notes);
+  res.json(parsedNotes);
+});
+
+app.post("/api/notes", async (req, res) => {
+  let notes = await readFromFile("db/db.json");
+  let parsedNotes = JSON.parse(notes);
+  let { title, text } = req.body;
+  let newNote = { id: uuid.v4(), title, text };
+  let updatedNotes = [...parsedNotes, newNote];
+  await writeToFile("db/db.json", JSON.stringify(updatedNotes));
+  let overwrittenNotes = await readFromFile("db/db.json");
+  res.json(overwrittenNotes);
+});
+
+app.listen(PORT, () =>
+  console.log("Serving static asset routes at port http://localhost:${PORT}")
+);
+
+module.exports = express;
